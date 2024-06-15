8552c7b409283fef6460f8eed4a8e85b74196c1a
Nesta-gitU <99024501+Nesta-gitU@users.noreply.github.com>
2024-06-15 21:04:28 +0200

    this is dumb

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -42,7 +42,7 @@ def load_new_data(args):
 
 
 def random_neighbor(args, input_user_news, input_news_user, news_len):
-    max_news_id = np.max([int(i) for i in input_news_user.keys()])
+    max_news_id = np.max([int(i) for i in input_news_user.keys()]) + 1
     user_news = np.zeros([max_news_id, args.news_neighbor], dtype=np.int32)
     for i in input_user_news:
         n_neighbors = len(input_user_news[i])
@@ -52,7 +52,7 @@ def random_neighbor(args, input_user_news, input_news_user, news_len):
             sampled_indices = np.random.choice(list(range(n_neighbors)), size=args.news_neighbor, replace=True)
         user_news[int(i)] = np.array([input_user_news[i][k] for k in sampled_indices])
 
-    max_user_id = np.max([int(i) for i in input_news_user.keys()])
+    max_user_id = np.max([int(i) for i in input_news_user.keys()]) + 1
     news_user = np.zeros([max_user_id, args.user_neighbor], dtype=np.int32)
     for i in input_news_user:
         n_neighbors = len(input_news_user[i])
