07b57688ba2cb1bc1d414d4e519eed958bf14334
Nesta-gitU <99024501+Nesta-gitU@users.noreply.github.com>
2024-06-21 14:05:17 +0200

    print number of keys for test set indexe s

--- src/data/ebnerd_dataset.py
+++ src/data/ebnerd_dataset.py
@@ -165,7 +165,7 @@ class EbnerdDataset(Dataset):
             replace_column(DEFAULT_INVIEW_ARTICLES_COL, False)
         if self.mode == "test":
             replace_column(DEFAULT_INVIEW_ARTICLES_COL, False)
-            print(self.df_behaviors[DEFAULT_INVIEW_ARTICLES_COL])
+            print(self.df_behaviors[DEFAULT_INVIEW_ARTICLES_COL].keys())
 
         self.article_df = self.article_df.with_columns(
             pl.col(DEFAULT_ARTICLE_ID_COL).apply(lambda article_id: article_id_to_index[int(article_id)]).alias(DEFAULT_ARTICLE_ID_COL)
