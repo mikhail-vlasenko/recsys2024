7bc66f5693c779a7de0161afee71724b8eb7d393
Nesta-gitU <99024501+Nesta-gitU@users.noreply.github.com>
2024-06-23 23:09:56 +0200

    eh

--- src/new_main.py
+++ src/new_main.py
@@ -132,7 +132,7 @@ def train_and_test(data_download_path: str, args):
 
     if args.checkpoint is not None:
         run = wandb.init(entity="inverse_rl", project="RecSys")
-        current_checkpoint = run.use_artifact(args.checkpoint, type="model")
+        current_checkpoint = run.use_artifact(args.checkpoint)
         checkpoint = current_checkpoint.download() / "model.ckpt"
         module = OriginalModule.load_from_checkpoint(checkpoint, net=net)
     else:
