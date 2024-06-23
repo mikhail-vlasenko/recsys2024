7e39a8240e500f4934867aec9cca07cb6560a5d4
Nesta-gitU <99024501+Nesta-gitU@users.noreply.github.com>
2024-06-23 23:00:08 +0200

    remove double type

--- src/utils/get_training_args.py
+++ src/utils/get_training_args.py
@@ -65,7 +65,7 @@ def get_training_args():
     parser.add_argument('--one_row_impression', action='store_true', default=False)
     parser.add_argument('--use_labeled_test_set', action='store_true', default=False)
     parser.add_argument('--labeled_test_set_split', type=float, default=0.5, help='The fraction we take of the labeled val set to make a test set')
-    parser.add_argument('--checkpoint_list', type=list, type=list_of_strings, default=None, help='list of wandbcheckpoints')
+    parser.add_argument('--checkpoint_list', type=list_of_strings, default=None, help='list of wandbcheckpoints')
     
     show_loss = True
     show_time = False
